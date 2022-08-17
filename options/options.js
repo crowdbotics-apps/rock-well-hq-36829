ec3a3a9253d48e07a58c87348996eee4cffcaa54
Crowdbotics <team@crowdbotics.com>
2022-08-17 16:30:24 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "rock_well_hq_36829",
+  url: "https://rock_well_hq_36829.botics.co",
+  api: "https://rock_well_hq_36829.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
